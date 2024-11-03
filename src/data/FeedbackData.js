3b14fc00afe0f501f20fe7100c66f1721be35613
sksksk2024 <157216629+sksksk2024@users.noreply.github.com>
2024-11-03 18:07:28 +0000

    initial commit

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+  {
+    id: 1,
+    rating: 10,
+    text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi, excepturi?',
+  },
+  {
+    id: 2,
+    rating: 9,
+    text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet, perferendis eos? Explicabo porro, necessitatibus nam suscipit nesciunt provident magni, pariatur ducimus quia saepe labore. Cumque quis nobis quo vel.',
+  },
+  {
+    id: 3,
+    rating: 8,
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Et nesciunt inventore hic a nihil eveniet illo, debitis ab totam minima!',
+  },
+]
+
+export default FeedbackData
